cf683229662cff0128a875073cc81cafdceb2b3b
yapeng <yapeng.gao@hotmail.com>
2022-06-29 15:10:44 +0800

    save model everytime

--- spinup/algos/pytorch/ppo/ppo.py
+++ spinup/algos/pytorch/ppo/ppo.py
@@ -331,7 +331,7 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
         # Save model
         if (epoch % save_freq == 0) or (epoch == epochs-1):
-            logger.save_state({'env': env}, None)
+            logger.save_state({'env': env}, epoch)
 
         # Perform PPO update!
         update()
